34a8f5731846676f5dfa79f158c75b8669ae2a71
iuliabac <baca0003@hz.nl>
2025-02-08 17:50:59 +0100

    added the pictures

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,36 +82,36 @@
         </nav>
         <nav>
             <img align="left"
-                 src="resources\nederlands.gif">
+                 src="resources\assets\nederlands.gif" alt="">
             <img align="Right"
-                 src="resources\nederlands.gif">
-            <h1 style="color: rgb(10, 30, 81)";>Welkom bij de index page van Bacanu Iulia</h1>
+                 src="resources\assets\nederlands.gif" alt="">
+            <h1 style="color: rgb(10, 30, 81)">Welkom bij de index page van Băcanu Iulia</h1>
 
             <img align="left"
-                 src="resources\Romania.gif">
+                 src="resources\assets\Romania.gif" alt="">
             <img align="Right"
-                 src="resources\Romania.gif">
-            <h1 style="color: rgb(10, 30, 81)";>Bun venit la pagina de index a lui Bacanu Iulia</h1>
+                 src="resources\assets\Romania.gif" alt="">
+            <h1 style="color: rgb(10, 30, 81)">Bun venit la pagina de index a lui Băcanu Iulia</h1>
 
             <img align="left"
-                 src="resources\uk_gs.gif">
+                 src="resources\assets\uk_gs.gif" alt="">
             <img align="Right"
-                 src="resources\uk_gs.gif">
-            <h1 style="color: rgb(10, 30, 81)";>Welcome to Bacanu Iulia's index page</h1>
+                 src="resources\assets\uk_gs.gif" alt="">
+            <h1 style="color: rgb(10, 30, 81)">Welcome to Băcanu Iulia's index page</h1>
         </nav>
     </head>
     <body style="background-color:rgb(197, 220, 247);">
 
     <img align="left"
-         src="resources\cat_md_clr.gif">
+         src="resources\assets\cat_md_clr.gif" alt="">
     <img align="Right"
-         src="resources\cat_md_clr.gif">
+         src="resources\assets\cat_md_clr.gif" alt="">
     <p>I choose ICT because I had prior experience with it since the first grade up until high school. I was naturally talented using the computer at a very young age. I think my motivation for ICT came from my parents as well as my former IT teachers as they would always support me in working with computers.</p>
     <p>During my school years, I would work a lot on various applications (Word, PowerPoint, Excel,). Though coding lessons were limited during my time at school, we would program simple websites using HTML as well as working on python using simple comands. and maybe, I could improve my coding skills during my time at this institution.</p>
     <a href="https://hz.nl/"><img
             src="https://iuliabac.github.io/hz-university-of-applied-sciences.svg"
             style="width:200px;
-                   height:auto;"></a>
+                   height:auto;" alt=""></a>
     </body>
 </main>
 </html>
