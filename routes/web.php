0dac514e0a86d89960b355eebc86acef69a606bb
iuliabac <baca0003@hz.nl>
2025-02-09 09:25:41 +0100

    Created routes

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,34 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/blog/feedback', function () {
+    return view('feedback');
+});
+
+Route::get('/blog/programming', function () {
+    return view('programming');
+});
+
+Route::get('/blog/study', function () {
+    return view('study');
+});
+
+Route::get('/blog/swot', function () {
+    return view('swot');
+});
