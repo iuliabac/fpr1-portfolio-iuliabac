fea36748a8b69024abdfb6411b0ee735493e10b5
iuliabac <baca0003@hz.nl>
2025-02-18 12:05:14 +0100

    updated the faq controller

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Faq;
 use Illuminate\Http\Request;
 
 class FaqController extends Controller
@@ -11,5 +12,65 @@ class FaqController extends Controller
         return view('faq', [
             'faq' => Faq::all()
         ]);
+    }
+
+    public function create()
+    {
+        return view('faq.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required',
+            'question' => 'required',
+            'answer' => 'required'
+        ]);
+
+        $faq = Faq::create($validated);
+
+        return redirect('/faq');
+    }
+
+    public function show(Faq $faq)
+    {
+        return view('faq.show', [
+            'faq' => $faq
+        ]);
+    }
+
+    public function edit(Faq $faq)
+    {
+        return view('faq.edit', [
+            'faq' => $faq
+        ]);
+    }
+
+    public function update(Request $request, Faq $faq)
+    {
+        $validated = $request->validate([
+            'title' => 'required',
+            'question' => 'required',
+            'answer' => 'required'
+        ]);
+
+        $faq -> update($validated);
+
+        return redirect('/faq', $faq)
+            ->with('success', 'Faq updated successfully');
+    }
+
+    public function delete(Faq $faq)
+    {
+        return view('faq.delete', [
+            'faq' => $faq
+        ]);
+    }
+
+    public function destroy(Faq $faq)
+    {
+        $faq->delete();
+        return redirect('/faq')
+            ->with('success', 'Faq deleted successfully');
     }
 }
